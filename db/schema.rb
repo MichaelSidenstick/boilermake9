862276ff8f4c620acde6611aff1f2098586a0128
MichaelSidenstick <michael.sidenstick@gmail.com>
2022-01-23 05:10:32 -0500

    Added views depending on Donor/Recipient status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_054222) do
+ActiveRecord::Schema.define(version: 2022_01_23_084730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2022_01_23_054222) do
     t.integer "requester_id"
     t.integer "donor_id"
     t.datetime "exp_date"
-
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
@@ -38,6 +37,10 @@ ActiveRecord::Schema.define(version: 2022_01_23_054222) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "productToken"
+    t.string "cartToken"
+    t.boolean "recipient"
+    t.string "userType"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
